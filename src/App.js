c1589888e99adc4d4042853ad7bbcd0af9b42ddb
Shawn Kohltfarber <shawnkohltfarber@Shawns-MacBook-Pro.local>
2020-07-27 21:16:37 -0500

    We moved the github pages into production.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './styles/index.css'
 
-function App() {
+import CameraFeed from './components/CameraFeed'
+import MintNav from './components/MintNav'
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <MintNav />
+      <div className="App">
+        <h1 className='mt-2 mb-2'>Using Camera API in Browser</h1>
+        <CameraFeed />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/MintNav.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import logo from '.././webicon.png';
+
+const MintNav = () => {
+  return (
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        src={logo}
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+      {' '}
+      Shawn Kohltfarber
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+    <Nav.Link target="_blank" rel="noopener noreferrer" href="https://github.com/skohltfarber/usingCamera">Repo</Nav.Link>
+    </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+  )
+}
+
+export default MintNav
