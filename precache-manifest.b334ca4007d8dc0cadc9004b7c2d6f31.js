3914c365467c42b434eddcc06ed13efba314a717
Shawn Kohltfarber <shawnkohltfarber@Shawns-MacBook-Pro.local>
2020-07-27 21:15:42 -0500

    Updates

---
+++ precache-manifest.b334ca4007d8dc0cadc9004b7c2d6f31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d4a55c3feb090686c24f0ec6579999",
+    "url": "/usingCamera/index.html"
+  },
+  {
+    "revision": "142f05446e7b7ba168b4",
+    "url": "/usingCamera/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d65c1588de19ca26594a",
+    "url": "/usingCamera/static/css/main.a0b33bfa.chunk.css"
+  },
+  {
+    "revision": "142f05446e7b7ba168b4",
+    "url": "/usingCamera/static/js/2.7cefd6a0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/usingCamera/static/js/2.7cefd6a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65c1588de19ca26594a",
+    "url": "/usingCamera/static/js/main.5f8ea4ed.chunk.js"
+  },
+  {
+    "revision": "01b3c333325ab85402d6",
+    "url": "/usingCamera/static/js/runtime-main.f05d3caf.js"
+  },
+  {
+    "revision": "e7ffdc6022d19e267bc9355e23dac3c2",
+    "url": "/usingCamera/static/media/webicon.e7ffdc60.png"
+  }
+]);
